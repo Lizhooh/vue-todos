53f919764b7788072aeedcd391ec00bbe052149d
Lizhooh <960316408@qq.com>
2017-02-04 15:41:12 +0800

    render by type

--- src/Todos/Box/Box.vue
+++ src/Todos/Box/Box.vue
@@ -16,12 +16,18 @@
                 >
         </header>
         <section>
-            <List :todos="todos" :hide="listHide"/>
+            <List :todos="computedTodos" :hide="listHide"/>
         </section>
         <footer>
-            <span class="todo-all-count">任务事项: {{ todos.length }}</span>
-            <span class="todo-active-count">活动事项: {{ todoActive.length }}</span>
-            <span class="todo-completed-count">完成事项: {{ todoCompleted.length }}</span>
+            <span class="todo-all-count" @click="showAll" :class="{'active': listState === 0}">
+                任务事项: {{ todos.length }}
+            </span>
+            <span class="todo-active-count" @click="showActive" :class="{'active': listState === 1}">
+                活动事项: {{ todoActive.length }}
+            </span>
+            <span class="todo-completed-count" @click="showCompleted" :class="{'active': listState === 2}">
+                完成事项: {{ todoCompleted.length }}
+            </span>
 
             <button class="todo-claer-completed" @click="clearCompleted">
                 {{ todoCompleted.length > 0 && '清空完成事项' || ''}}
@@ -45,6 +51,7 @@
         data() {
             return {
                 listHide: false,
+                listState: 0,
             };
         },
         computed: {
@@ -54,6 +61,11 @@
             todoCompleted() {
                  return this.todos.filter(todo => todo.completed === true);
             },
+            computedTodos() {
+                if(this.listState === 0) return this.todos;
+                if(this.listState === 1) return this.todoActive;
+                if(this.listState === 2) return this.todoCompleted;
+            }
         },
         methods: {
             submit(event) {
@@ -67,6 +79,15 @@
             },
             clearCompleted(event) {
                 Todos.clearCompleted();
+            },
+            showAll(event) {
+                this.listState = 0;
+            },
+            showActive(event) {
+                this.listState = 1;
+            },
+            showCompleted(event) {
+                this.listState = 2;
             },
         }
     };
